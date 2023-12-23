7268f85f4817c33080f0f307c5e9c3a44d2d4f8a
Wolfgang Maier <w.maier74@gmx.de>
2023-12-23 12:05:37 +0100

    CamelCase

---
+++ FlowProtocol2/Commands/CmdCamelCase.cs
@@ -0,0 +1,61 @@
+namespace FlowProtocol2.Commands
+{
+    using System.Text.RegularExpressions;
+    using FlowProtocol2.Core;
+
+    /// <summary>
+    /// Implementiert den CamelCase-Befehl
+    /// </summary>
+    public class CmdCamelCase : CmdBaseCommand
+    {
+        public string VarName { get; set; }
+        public string Text { get; set; }
+
+        public static CommandParser GetComandParser()
+        {
+            return new CommandParser(@"^~CamelCase ([A-Za-z0-9]*)\s*=(.*)", (rc, m) => CreateCamelCaseCommand(rc, m));
+        }
+
+        private static CmdBaseCommand CreateCamelCaseCommand(ReadContext rc, Match m)
+        {
+            CmdCamelCase cmd = new CmdCamelCase(rc);
+            cmd.VarName = m.Groups[1].Value.Trim();
+            cmd.Text = m.Groups[2].Value.Trim();
+            return cmd;
+        }
+
+        public CmdCamelCase(ReadContext readcontext) : base(readcontext)
+        {
+            VarName = string.Empty;
+            Text = string.Empty;
+        }
+
+        public override CmdBaseCommand? Run(RunContext rc)
+        {
+            string ccwert = ReplaceVars(rc, Text.Trim());
+            ccwert = ccwert.Replace("ä", "ae", false, null)
+                           .Replace("ö", "oe", false, null)
+                           .Replace("ü", "ue", false, null)
+                           .Replace("Ä", "Ae", false, null)
+                           .Replace("Ö", "Oe", false, null)
+                           .Replace("Ü", "Ue", false, null)
+                           .Replace("ß", "ss", false, null);
+            ccwert = Regex.Replace(ccwert, @"[^\w]", "_");
+            ccwert = Regex.Replace(ccwert, @"__*", "_");
+            while (ccwert.Contains('_'))
+            {
+                int pos = ccwert.IndexOf('_');
+                if (pos + 2 < ccwert.Length)
+                {
+                    ccwert = ccwert.Substring(0, pos) + ccwert.Substring(pos + 1, 1).ToUpper() + ccwert.Substring(pos + 2);
+                }
+                else
+                {
+                    ccwert = ccwert.Replace("_", "");
+                }
+            }
+            rc.InternalVars[VarName] = ccwert;
+            return NextCommand;
+        }
+    }
+}
--- FlowProtocol2/Core/ScriptParser.cs
+++ FlowProtocol2/Core/ScriptParser.cs
@@ -32,6 +32,7 @@ namespace FlowProtocol2.Core
             CmdParser.Add(CmdExecute.GetComandParser());
             CmdParser.Add(CmdIf.GetComandParser());
             CmdParser.Add(CmdElse.GetComandParser());
+            CmdParser.Add(CmdCamelCase.GetComandParser());
             // Hier weitere Parser hinzufügen
         }
 

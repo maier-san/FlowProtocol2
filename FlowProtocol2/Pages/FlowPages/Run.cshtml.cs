e5fdaeb7eb76f23b098669ff36d38c8eb2728e95
Wolfgang Maier <w.maier74@gmx.de>
2024-04-01 11:22:35 +0200

    BoundVar-Preload

--- FlowProtocol2/Pages/FlowPages/Run.cshtml.cs
+++ FlowProtocol2/Pages/FlowPages/Run.cshtml.cs
@@ -21,7 +21,7 @@ namespace FlowProtocol2.Pages.FlowPages
 
         public RunModel(IConfiguration configuration)
         {
-            ScriptPath = configuration["ScriptPath"];                    
+            ScriptPath = configuration["ScriptPath"];
             ScriptFilePath = string.Empty;
             ScriptName = string.Empty;
             ScriptBaseURL = string.Empty;
@@ -49,11 +49,18 @@ namespace FlowProtocol2.Pages.FlowPages
             var sinfo = sp.ReadScript(RunContext, ScriptFilePath, 0);
             ScriptRunner sr = new ScriptRunner();
             RunContext.BoundVars = BoundVars;
-            RunContext.MyDomain =this.HttpContext.Request.Scheme + "://" + this.HttpContext.Request.Host;
+            RunContext.MyDomain = this.HttpContext.Request.Scheme + "://" + this.HttpContext.Request.Host;
             RunContext.MyBaseURL = RunContext.MyDomain + this.HttpContext.Request.Path;
             RunContext.MyResultURL = RunContext.MyBaseURL + this.HttpContext.Request.QueryString;
+
+            // Alle gebundenen Variablen ohne BaseKey-Anteil als interne Variablen verfügbar machen:
+            foreach (var p in RunContext.BoundVars)
+            {
+                if (!string.IsNullOrEmpty(p.Value) && !p.Value.Contains('_')) RunContext.InternalVars[p.Key] = p.Value;
+            }
+
             sr.RunScript(RunContext, sinfo.StartCommand);
-            
+
             // Füge die restlichen gesetzten Parameter als gegeben dazu:
             RunContext.GivenKeys.AddRange(
                 RunContext.BoundVars
